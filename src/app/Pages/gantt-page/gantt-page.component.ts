dc012aab952fcee6df56baa81790e0e4c7e7bdd4
Youssef Outahar <youssefoutahar1@gmail.com>
2023-12-17 13:39:49 +0100

    gantt chart

--- src/app/Pages/gantt-page/gantt-page.component.ts
+++ src/app/Pages/gantt-page/gantt-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Gantt, GanttStatic, gantt } from 'dhtmlx-gantt';
+import { TaskService } from '../../Services/Tasks/tasks.service';
+import { LinkService } from '../../Services/Tasks/link.service';
 
 @Component({
   selector: 'app-gantt-page',
+
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './gantt-page.component.html',
-  styleUrls: ['./gantt-page.component.css']
+  styleUrls: ['./gantt-page.component.css'],
+  providers: [TaskService, LinkService],
 })
 export class GanttPageComponent implements OnInit {
+  @ViewChild('gantt_here', { static: true }) ganttContainer!: ElementRef;
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private linkService: LinkService
+  ) {}
 
   ngOnInit() {
-  }
+    gantt.config.date_format = '%Y-%m-%d %H:%i';
 
+    gantt.init(this.ganttContainer.nativeElement);
+
+    Promise.all([this.taskService.get(), this.linkService.get()]).then(
+      ([data, links]) => {
+        gantt.parse({ data, links });
+      }
+    );
+  }
 }
---
+++ src/app/Services/Tasks/link.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Link } from './Interfaces';
+
+@Injectable()
+export class LinkService {
+  get(): Promise<Link[]> {
+    return Promise.resolve([{ id: 1, source: 1, target: 2, type: '0' }]);
+  }
+}
